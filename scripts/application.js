4ec8d41a4fd5b8e094f6caf66bc9a55f2e82dec9
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2019-04-25 02:03:46 +0300

    Güncelleme

--- scripts/application.js
+++ scripts/application.js
@@ -1,13 +1,3 @@
-/**
- * @name            QPanel
- * @name            application.js
- * @version         1.0.0
- * @description     Bootstrap Admin Panel
- * @repository      https://github.com/emrecanoztas-com/QPanel
- * @author          Emre Can ÖZTAŞ (ecoztas) <me@emrecanoztas.com>
- * @license         MIT
- */
-
 $(document).ready(function() {
     // Sidebar aç / kapat
     $('#launcher').click(function() {
